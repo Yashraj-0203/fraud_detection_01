e1c3be6f483ce1be5fec607fd6d8dd36730e0663
Yashraj-0203 <yashraj_pratap@yahoo.com>
2023-02-21 21:13:43 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,13 @@
 ml_model.py
 
 print("adding all ML related code here")
+
+def model1():
+    print("adding code for ML MOdel1")
+
+def model2():
+    print("adding code for ML MOdel2")
+
+def model3():
+    print("adding code for ML MOdel3")
+
